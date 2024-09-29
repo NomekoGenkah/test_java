e39d24e5bea7f6fe11470b5c24401fc53c92fa34
NomekoGenkah <166954170+NomekoGenkah@users.noreply.github.com>
2024-09-29 20:39:27 -0300

    implementacion buscar() seleccion multiple

--- src/proyecto/Selec_Mul_Pregunta.java
+++ src/proyecto/Selec_Mul_Pregunta.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package proyecto;
+import java.util.Scanner;
 
 /**
  *
@@ -12,13 +13,30 @@ public class Selec_Mul_Pregunta extends Pregunta{
     String[] opciones;
     int index;
     
-    Selec_Mul_Pregunta(int peso, String pregunta){
+    Selec_Mul_Pregunta(int peso, String pregunta, String[] opciones, int index){
         super(peso, pregunta);   
+        this.opciones = opciones;
+        this.index = index;
     }
     
     
     @Override
     boolean buscar(){
-        return true;   
+        int valorLetra = 97, opcionUsuario;
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.println(super.text);
+        for(String opcion: this.opciones){
+            System.out.println((char) valorLetra + ".)" + opcion);
+            valorLetra++;
+        }
+
+        opcionUsuario = teclado.nextInt();
+        teclado.close();
+
+        if(this.index == opcionUsuario){
+            return true;
+        }
+        return false;
     } 
 }

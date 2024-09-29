183bcdc766dfa65b27b3372d91411b488dd0f0d5
NomekoGenkah <166954170+NomekoGenkah@users.noreply.github.com>
2024-09-29 19:53:10 -0300

    Implementacion buscar() de preguntas cortas

--- src/proyecto/Resp_Cortas_Pregunta.java
+++ src/proyecto/Resp_Cortas_Pregunta.java
@@ -3,21 +3,30 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package proyecto;
+import java.util.Scanner;
 
 /**
  *
  * @author pedro
  */
 public class Resp_Cortas_Pregunta extends Pregunta {
-    String respuestaCorrecta;
+    String respuesta;
     
-    Resp_Cortas_Pregunta(int peso, String pregunta, String respuestaCorrecta){
+    Resp_Cortas_Pregunta(int peso, String pregunta, String respuesta){
         super(peso, pregunta);
-        this.respuestaCorrecta = respuestaCorrecta;
+        this.respuesta = respuesta;
     }
     
     @Override
     boolean buscar(){
-        return true;
+        Scanner teclado = new Scanner(System.in);
+        String respuestaUsuario;
+
+        System.out.print(super.text);
+        System.out.println("Ingrese su respuesta: ");
+
+        respuestaUsuario = teclado.nextLine();
+        teclado.close();
+        return this.respuesta.equalsIgnoreCase(respuestaUsuario);
     }
 }

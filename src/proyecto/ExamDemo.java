7ac5ee3258d741cd5cf35f5e0c66fb2b332e1472
NomekoGenkah <166954170+NomekoGenkah@users.noreply.github.com>
2024-10-04 10:31:00 -0300

    Demo incompleto

--- src/proyecto/ExamDemo.java
+++ src/proyecto/ExamDemo.java
@@ -3,10 +3,13 @@ package proyecto;
 public class ExamDemo {
 
     public static void main(String args[]){
+        String[] opciones = {"santiago", "abril", "pcausa"};
+        
 
         Exam exam = new Exam();
         exam.agregarPregunta(new TFpregunta("La capital de Chile es Santiago", true, 1));
         exam.agregarPregunta(new Resp_Cortas_Pregunta("quien?", "queso", 3));
+        exam.agregarPregunta(new Selec_Mul_Pregunta("que onda", opciones, 2, 2));
         int puntaje = exam.darExam();
         System.out.print(puntaje);
 

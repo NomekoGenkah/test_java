61d6a509c92fd173aa2c318c6ea9481ad46b56c7
Pedro Rojas <166954170+NomekoGenkah@users.noreply.github.com>
2024-10-21 18:20:32 -0300

    Update ExamDemo.java

--- src/proyecto/ExamDemo.java
+++ src/proyecto/ExamDemo.java
@@ -3,17 +3,27 @@ package proyecto;
 public class ExamDemo {
 
     public static void main(String args[]){
-        String[] opciones = {"santiago", "abril", "pcausa"};
+    //    String[] opciones = {"santiago", "abril", "pcausa"};
+        Exam miExam = new Exam();
+
+        miExam.agregaPregunta(new TFpregunta("La capital de Chile es Santiago", true, 1));
         
-
-        Exam exam = new Exam();
-        exam.agregarPregunta(new TFpregunta("La capital de Chile es Santiago", true, 1));
-        exam.agregarPregunta(new Resp_Cortas_Pregunta("quien?", "queso", 3));
-        exam.agregarPregunta(new Selec_Mul_Pregunta("que onda", opciones, 2, 2));
-        int puntaje = exam.darExam();
+        String santiagoElec[] = {"Santiago", "Valparaiso", "Concepcion", "Magallanes", "Arica"};
+        miExam.agregaPregunta(new Selec_Mul_Pregunta("Cuál es la capital de Region Metropolitana", santiagoElec, 0, 1));
+        
+        miExam.agregaPregunta(new Resp_Cortas_Pregunta("Cuál es la capital de IV Region","Coquimbo", 1));
+        miExam.agregaPregunta(new TFpregunta("La capital de Alberta es Calgary", false, 1));
+        String BCElec[] = {"Victoria", "Vancouver", "Nanaimo"};
+        miExam.agregaPregunta(new Selec_Mul_Pregunta("Cuál es la capital de British Columbia",
+        BCElec, 0, 1));
+        miExam.agregaPregunta(new Resp_Cortas_Pregunta("Cuál es la capital de Argentina","Buenos Aires", 1));
+        miExam.agregaPregunta(new Resp_Cortas_Pregunta("Cuál es la capital de Canada", "Ottawa",2));
+        miExam.agregaPregunta(new TFpregunta("La capital de La Serena es Illapel", false, 1));
+        String PElec[] = {"Brasilia", "Rio de Janeiro", "Sao Paulo", "Blumenau"};
+        miExam.agregaPregunta(new Selec_Mul_Pregunta("Cuál es la capital de Brasil?", PElec, 0, 1));
+        int puntaje = miExam.darExam();
+        System.out.println(puntaje);
+        
         System.out.print(puntaje);
-
-
-    }
-    
+    }   
 }
